a8d239486626c1f7ebb31f336f35095460dbc659
silvan <dh502240422@gmail.com>
2017-09-24 14:08:25 +0800

    1

--- app/Http/Controllers/WeixinController.php
+++ app/Http/Controllers/WeixinController.php
@@ -12,8 +12,84 @@ class weixinController extends Controller
      * @return \Illuminate\Http\Response
      */
 
-    //检查签名
-    private function checkSignature()
+
+
+    //验证消息
+    public function api()
+    {
+        $echoStr = $_GET["echostr"];
+        if ($this->checkSignature()) {
+            echo $echoStr;
+            exit;
+        }
+    }
+//        //get post data, May be due to the different environments
+//        $postStr = $GLOBALS["HTTP_RAW_POST_DATA"];//php:input
+//        //写入日志  在同级目录下建立php_log.txt
+//        //chmod 777php_log.txt(赋权) chown wwwphp_log.txt(修改主)
+//        error_log(var_export($postStr,1),3,'php_log.txt');
+//        //日志图片
+//
+//        //extract post data
+//      //  if (!empty($postStr)){
+//            /* libxml_disable_entity_loader is to prevent XML eXternal Entity Injection,
+//               the best way is to check the validity of xml by yourself */
+//            libxml_disable_entity_loader(true);
+//            $postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
+//            $fromUsername = $postObj->FromUserName;
+//            $toUsername = $postObj->ToUserName;
+//            $keyword = trim($postObj->Content);
+//            $msgType = $postObj->MsgType;//消息类型
+//            $event = $postObj->Event;//时间类型，subscribe（订阅）、unsubscribe（取消订阅）
+//            $time = time();
+//            $textTpl = "<xml>
+//                           <ToUserName><![CDATA[%s]]></ToUserName>
+//                           <FromUserName><![CDATA[%s]]></FromUserName>
+//                           <CreateTime>%s</CreateTime>
+//                           <MsgType><![CDATA[%s]]></MsgType>
+//                           <Content><![CDATA[%s]]></Content>
+//                           <FuncFlag>0</FuncFlag>
+//                           </xml>";
+//            //订阅事件
+//            if($postObj->Event=="subscribe")
+//            {
+//                $msgType = "text";
+//                $contentStr = "欢迎关注silvan";
+//                $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
+//                echo $resultStr;
+//
+//            }
+//
+//
+//            //语音识别
+//            if($postObj->MsgType=="voice"){
+//                $msgType = "text";
+//                $contentStr = trim($postObj->Recognition,"。");
+//                $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
+//                echo  $resultStr;
+//
+//            }
+//
+//            //自动回复
+//            if(!empty( $keyword ))
+//            {
+//                $msgType = "text";
+//                $contentStr = "学习测试中！";
+//                $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
+//                echo $resultStr;
+//
+//            }else{
+//                echo "Input something...";
+//
+//            }
+
+//        }else {
+//            echo "";
+//            exit;
+//        }
+
+        //检查签名
+        private function checkSignature()
     {
         $signature = $_GET["signature"];
         $timestamp = $_GET["timestamp"];
@@ -30,77 +106,7 @@ class weixinController extends Controller
         }
     }
 
-    //验证消息
-    public function api()
-    {
-        //get post data, May be due to the different environments
-        $postStr = $GLOBALS["HTTP_RAW_POST_DATA"];//php:input
-        //写入日志  在同级目录下建立php_log.txt
-        //chmod 777php_log.txt(赋权) chown wwwphp_log.txt(修改主)
-        error_log(var_export($postStr,1),3,'php_log.txt');
-        //日志图片
-
-        //extract post data
-      //  if (!empty($postStr)){
-            /* libxml_disable_entity_loader is to prevent XML eXternal Entity Injection,
-               the best way is to check the validity of xml by yourself */
-            libxml_disable_entity_loader(true);
-            $postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
-            $fromUsername = $postObj->FromUserName;
-            $toUsername = $postObj->ToUserName;
-            $keyword = trim($postObj->Content);
-            $msgType = $postObj->MsgType;//消息类型
-            $event = $postObj->Event;//时间类型，subscribe（订阅）、unsubscribe（取消订阅）
-            $time = time();
-            $textTpl = "<xml>
-                           <ToUserName><![CDATA[%s]]></ToUserName>
-                           <FromUserName><![CDATA[%s]]></FromUserName>
-                           <CreateTime>%s</CreateTime>
-                           <MsgType><![CDATA[%s]]></MsgType>
-                           <Content><![CDATA[%s]]></Content>
-                           <FuncFlag>0</FuncFlag>
-                           </xml>";
-            //订阅事件
-            if($postObj->Event=="subscribe")
-            {
-                $msgType = "text";
-                $contentStr = "欢迎关注silvan";
-                $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
-                echo $resultStr;
-
-            }
 
 
-            //语音识别
-            if($postObj->MsgType=="voice"){
-                $msgType = "text";
-                $contentStr = trim($postObj->Recognition,"。");
-                $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
-                echo  $resultStr;
-
-            }
-
-            //自动回复
-            if(!empty( $keyword ))
-            {
-                $msgType = "text";
-                $contentStr = "学习测试中！";
-                $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
-                echo $resultStr;
-
-            }else{
-                echo "Input something...";
-
-            }
-
-//        }else {
-//            echo "";
-//            exit;
-//        }
-
-
-
-
-    }
     //响应消息
 }
