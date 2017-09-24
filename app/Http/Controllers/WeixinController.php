8b0485169b08ea4ccc9151486639c2c49063478f
silvan <dh502240422@gmail.com>
2017-09-24 12:48:26 +0800

    1

--- app/Http/Controllers/WeixinController.php
+++ app/Http/Controllers/WeixinController.php
@@ -53,8 +53,8 @@ class weixinController extends Controller
             $event = $postObj->Event;//时间类型，subscribe（订阅）、unsubscribe（取消订阅）
             $time = time();
             $textTpl = "<xml>
-                           <ToUserName><![CDATA[".$fromUsername."]]></ToUserName>
-                           <FromUserName><![CDATA[".$toUsername."]]></FromUserName>
+                           <ToUserName><![CDATA[%s]]></ToUserName>
+                           <FromUserName><![CDATA[%s]]></FromUserName>
                            <CreateTime>%s</CreateTime>
                            <MsgType><![CDATA[%s]]></MsgType>
                            <Content><![CDATA[%s]]></Content>
