d6f0c0a5a965cf6a8eaecd23de70d62d65bfe2ee
silvan <dh502240422@gmail.com>
2017-09-26 00:27:10 +0800

    1

--- app/Http/Controllers/WeixinController.php
+++ app/Http/Controllers/WeixinController.php
@@ -52,44 +52,52 @@ class weixinController extends Controller
                            <FuncFlag>0</FuncFlag>
                            </xml>";
 
-            $newsTplHead = "<xml>
-                               <ToUserName><![CDATA[%s]]></ToUserName>
-                               <FromUserName><![CDATA[%s]]></FromUserName>
-                               <CreateTime>%s</CreateTime>
-                               <MsgType><![CDATA[%s]]></MsgType>
-                               <ArticleCount>2</ArticleCount>
-                               <Articles>";
+            $newsTpl= "<xml>
+                           <ToUserName><![CDATA[%s]]></ToUserName>
+                           <FromUserName><![CDATA[%s]]></FromUserName>
+                           <CreateTime>%s</CreateTime>
+                           <MsgType><![CDATA[%s]]></MsgType>
+                           <ArticleCount>2</ArticleCount>
+                           <Articles>
+                           <item>
+                           <Title><![CDATA[%s]]></Title>
+                           <Description><![CDATA[%s]]></Description>
+                           <PicUrl><![CDATA[%s]]></PicUrl>
+                           <Url><![CDATA[%s]]></Url>
+                           </item>
+                           </Articles>
+                           </xml>";
                             
-            $newsTplBody = "<item>
-                <Title><![CDATA[%s]]></Title>
-                <Description><![CDATA[%s]]></Description>
-                <PicUrl><![CDATA[%s]]></PicUrl>
-                 <Url><![CDATA[%s]]></Url>
-                </item>";
-            $newsTplBody2 = "<item>
-                <Title><![CDATA[%s]]></Title>
-                <Description><![CDATA[%s]]></Description>
-                <PicUrl><![CDATA[%s]]></PicUrl>
-                 <Url><![CDATA[%s]]></Url>
-                </item>";
-            $newsTplFoot = "</Articles>
-                </xml>";
+//            $newsTplBody = "<item>
+//                <Title><![CDATA[%s]]></Title>
+//                <Description><![CDATA[%s]]></Description>
+//                <PicUrl><![CDATA[%s]]></PicUrl>
+//                 <Url><![CDATA[%s]]></Url>
+//                </item>";
+//            $newsTplBody2 = "<item>
+//                <Title><![CDATA[%s]]></Title>
+//                <Description><![CDATA[%s]]></Description>
+//                <PicUrl><![CDATA[%s]]></PicUrl>
+//                 <Url><![CDATA[%s]]></Url>
+//                </item>";
+//            $newsTplFoot = "</Articles>
+//                </xml>";
 
-            $url = 'https://api.douban.com/v2/movie/in_theaters?count=10';
-            $result = file_get_contents($url);
-            $jsonArray = json_decode($result,true);
-            $a['title']=$jsonArray['subjects'][0]['title'];
-            $c['large']=$jsonArray['subjects'][0]['images']['large'];
-            $a1['title']=$jsonArray['subjects'][1]['title'];
-            $c1['large']=$jsonArray['subjects'][1]['images']['large'];
-            $url1 = 'https://api.douban.com/v2/movie/subject/25808075';
-            $result1 = file_get_contents($url1);
-            $jsonArray1 = json_decode($result1,true);
-            $b['summary']=$jsonArray1['summary'];
-            $url2 = 'https://api.douban.com/v2/movie/subject/22266126';
-            $result1 = file_get_contents($url2);
-            $jsonArray1 = json_decode($result1,true);
-            $b1['summary']=$jsonArray1['summary'];
+//            $url = 'https://api.douban.com/v2/movie/in_theaters?count=10';
+//            $result = file_get_contents($url);
+//            $jsonArray = json_decode($result,true);
+//            $a['title']=$jsonArray['subjects'][0]['title'];
+//            $c['large']=$jsonArray['subjects'][0]['images']['large'];
+          //  $a1['title']=$jsonArray['subjects'][1]['title'];
+           // $c1['large']=$jsonArray['subjects'][1]['images']['large'];
+//            $url1 = 'https://api.douban.com/v2/movie/subject/25808075';
+//            $result1 = file_get_contents($url1);
+//            $jsonArray1 = json_decode($result1,true);
+//            $b['summary']=$jsonArray1['summary'];
+//            $url2 = 'https://api.douban.com/v2/movie/subject/22266126';
+//            $result1 = file_get_contents($url2);
+//            $jsonArray1 = json_decode($result1,true);
+        //    $b1['summary']=$jsonArray1['summary'];
 //            for($i=1;$i<=9;$i++){
 //
 //                //  $dh['title']=;
@@ -119,29 +127,24 @@ class weixinController extends Controller
             if (!empty($keyword)) {
                 if($keyword=="电影预告"){
                     $msgType="news";
-                    $title = $a['title'];
-                    $desc = $b['summary'];
-                    $picUrl = $c['large'];
-                    $Url='https://movie.douban.com/subject/25808075/mobile';
-                    //$results = sprintf($newsTplHead, $fromUsername,$toUsername,$time,$msgType,$title, $desc, $picUrl,$Url);
-                    $header=sprintf($newsTplHead, $fromUsername,$toUsername,$time,$msgType);
-                    $body=sprintf($newsTplBody,$title, $desc, $picUrl,$Url);
-
-                    $title = $a1['title'];
-                    $desc = $b1['summary'];
-                    $picUrl = $c1['large'];
-                    $Url='https://movie.douban.com/subject/22266126/mobile';
-                    $body1=sprintf($newsTplBody2,$title, $desc, $picUrl,$Url);
+                    $title = $this->searchMovieTitle($keyword);
+                    $desc = $this->searchMovieSummary($keyword);
+                    $picUrl = $this->searchMovieImage($keyword);
+                    $Url = $this->searchMovieMobileUrl($keyword);
+                    $results = sprintf($newsTpl, $fromUsername,$toUsername,$time,$msgType,$title, $desc, $picUrl,$Url);
+                    echo  $results;
+                   // $header=sprintf($newsTplHead, $fromUsername,$toUsername,$time,$msgType);
+                  //  $body=sprintf($newsTplBody,$title, $desc, $picUrl,$Url);
                     //   $url = $newsContent['url'];
                     //  $body = sprintf($newsTplBody, $title, $desc, $picUrl);
 
-                    $FuncFlag = 0;
+                    //$FuncFlag = 0;
                     // $footer = sprintf($newsTplFoot, $FuncFlag);
-                     $results=$header.$body.$body1.$newsTplFoot;
-                    echo  $results;
+                     //$results=$header.$body.$body1.$newsTplFoot;
+
                 }else{
                     $msgType = "text";
-                    $contentStr = "学习测试中！";
+                    $contentStr = "非常抱歉，没有搜索您的电影！";
                     $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
                     echo $resultStr;
                 }
@@ -176,6 +179,44 @@ class weixinController extends Controller
         }else{
             return false;
         }
+    }
+
+    public  function  searchMovieId($movie){
+        $UrlSr='https://api.douban.com/v2/movie/search?q='.$movie;
+        $result = file_get_contents($UrlSr);
+        $jsonArray = json_decode($result,true);
+        $id=$jsonArray['subjects'][0]['id'];
+        return  $id;
+    }
+
+    public function   searchMovieSummary($id){
+        $UrlSr='https://api.douban.com/v2/movie/subject/'.$id;
+        $result = file_get_contents($UrlSr);
+        $jsonArray = json_decode($result,true);
+        $summary=$jsonArray['summary'];
+        return  $summary;
+    }
+
+    public  function   searchMovieTitle($movie){
+        $UrlSr='https://api.douban.com/v2/movie/search?q='.$movie;
+        $result = file_get_contents($UrlSr);
+        $jsonArray = json_decode($result,true);
+        $title=$jsonArray['title'];
+        return $title;
+    }
+    public  function   searchMovieImage($movie){
+        $UrlSr='https://api.douban.com/v2/movie/search?q='.$movie;
+        $result = file_get_contents($UrlSr);
+        $jsonArray = json_decode($result,true);
+        $image_Url=$jsonArray['images']['large'];
+        return $image_Url;
+    }
+    public  function   searchMovieMobileUrl($movie){
+        $UrlSr='https://api.douban.com/v2/movie/search?q='.$movie;
+        $result = file_get_contents($UrlSr);
+        $jsonArray = json_decode($result,true);
+        $mobile_url=$jsonArray['mobile_url'];
+        return  $mobile_url;
     }
 
 
